0bf949cf1849b7e1c15976a878c5ac9b4ec0c794
Natalia0727 <oicafn@gmail.com>
2018-06-12 21:34:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
-require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+  get '/' do 
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is Natalia"
+  end 
+  get '/hometown' do
+  "My hometown is Las Vegas"
+  end 
+  get '/favorite-song' do 
+  "My favorite song is Bloom"
+  end
 end
